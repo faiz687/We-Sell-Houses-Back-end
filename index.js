6936233dba4f90b8b3a329ff68d6ef5d7b632b64
faizaan chowdhary <chowdhaf@uni.coventry.ac.uk>
2020-11-07 19:50:31 +0000

    feature table added and post request working

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const app = new Koa();
 app.use(cors());
 
 const users = require('./routes/Users.js');
+const property = require('./routes/Property.js');
 
 app.use(users.routes());
+app.use(property.routes());
 
 let port = process.env.PORT || 8000;
 
---
+++ models/Feature.js
@@ -0,0 +1,50 @@
+const db = require('../helpers/database');
+
+//get a single article by its id  
+exports.getById = async function getById (id) {
+  const query = "SELECT * FROM articles WHERE ID = ?;";
+  const values = [id];
+  const data = await db.run_query(query, values);
+  return data;
+}
+
+//list all the articles in the database
+exports.getAll = async function getAll (page, limit, order, direction) {
+  const offset = (page - 1) * limit;
+  let query;
+  if (direction === 'DESC') {
+    query = "SELECT * FROM articles ORDER BY ?? DESC LIMIT ? OFFSET ?;";
+  } else {
+    query = "SELECT * FROM articles ORDER BY ?? ASC LIMIT ? OFFSET ?;";    
+  }
+  const values = [order, parseInt(limit), parseInt(offset)];
+  const data = await db.run_query(query, values);
+  return data;
+}
+
+//create a new article in the database
+exports.add = async function add(houseid,body) {
+  body.features.forEach( async (item, index) => {
+    const query = "INSERT INTO Feature SET ?";
+    const feature = {houseid:houseid , feature:item }
+    const data = await db.run_query(query, feature);
+    return data;
+   
+  })
+}
+
+//delete an article by its id
+exports.delById = async function delById (id) {
+  const query = "DELETE FROM articles WHERE ID = ?;";
+  const values = [id];
+  const data = await db.run_query(query, values);
+  return data;
+}
+
+//update an existing article
+exports.update = async function update (article) {
+  const query = "UPDATE articles SET ? WHERE ID = ?;";
+  const values = [article, article.ID];
+  const data = await db.run_query(query, values);
+  return data;
+}
