d00b3f5dd5465c07c898f5615f192f7ae3e8b130
faizaan chowdhary <chowdhaf@uni.coventry.ac.uk>
2020-12-04 16:54:38 +0000

    working on Jsdocumentation

--- sql_scripts/Create_HouseFeatures.sql
+++ sql_scripts/Create_HouseFeatures.sql
@@ -2,5 +2,5 @@ create table Feature
 (
     FeatureId int auto_increment primary key,
     houseid   int         not null,
-    feature   varchar(30) not null
+    feature    varchar(30) not null
 );
